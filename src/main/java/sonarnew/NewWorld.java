4279b6610dbfcea4d978aafd9452af76fd9ec9bc
sunayanavaghela <sunayana.vag@gmail.com>
2021-08-30 17:54:04 +1000

    test again 1

--- src/main/java/sonarnew/NewWorld.java
+++ src/main/java/sonarnew/NewWorld.java
@@ -2,8 +2,5 @@ package sonarnew;
 
 public class NewWorld {
         public static void main (String args[]) {
-            int a = 10;
-            int b = 100;
-            int c= a + b;
     }
 }
