61553a65419810d1afa6fad2b14c943c3e917c6e
sunayanavaghela <sunayana.vag@gmail.com>
2021-09-01 13:50:23 +1000

    check different settings 10

--- src/main/java/sonarnew/NewWorld.java
+++ src/main/java/sonarnew/NewWorld.java
@@ -4,6 +4,5 @@ public class NewWorld {
         public static void main (String args[]) {
             String c = args[0] + args[1];
             System.out.println("Hello World again 8" + c);
-            System.out.println("This is a new comment" + c);
     }
 }
