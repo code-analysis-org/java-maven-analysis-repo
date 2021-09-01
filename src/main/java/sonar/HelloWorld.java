a083b87166e3c4ba49f541f3f0cd73c6f7fa38a9
sunayanavaghela <sunayana.vag@gmail.com>
2021-09-01 14:00:01 +1000

    check different settings 13

--- src/main/java/sonar/HelloWorld.java
+++ src/main/java/sonar/HelloWorld.java
@@ -3,6 +3,6 @@ package sonar;
 public class HelloWorld {
         public static void main (String args[]) {
             System.out.println("Hello World again 8. This is a new comment");
-            System.out.println("This is a new comment");
+            System.out.println("This is a new comment 1");
     }
 }
